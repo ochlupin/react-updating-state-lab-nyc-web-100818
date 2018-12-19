96b5cc758489b69559e963b9f6f470880c7f13fa
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-12-18 20:12:51 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+
+import React from "react";
+
+class DigitalClicker extends React.Component {
+  state = { timesClicked: 0 };
+
+  handleClick = () => {
+    console.log("State being updated!");
+    this.setState(currentState => ({
+      timesClicked: currentState.timesClicked + 1
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+      </div>
+    );
+  }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,54 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from "react";
+
+export default class YouTubeDebugger extends Component {
+  state = {
+    errors: [],
+    user: null,
+    settings: {
+      bitrate: 8,
+      video: {
+        resolution: "1080p"
+      }
+    }
+  };
+
+  handleClickBitrate = () => {
+    console.log("State => bitrate being updated");
+    this.setState({
+      settings: {
+        // use spread operator vs. that abomination known as Object.assign()
+        // spread operator essentially makes a copy of the array without mutating it
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  };
+
+  handleClickRes = () => {
+    console.log("State => resolution being updated");
+
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: "720p"
+        }
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button className="bitrate" onClick={this.handleClickBitrate}>
+          Set Bitrate to 12
+        </button>
+        <button className="resolution" onClick={this.handleClickRes}>
+          Set Resolution to 720p
+        </button>
+      </div>
+    );
+  }
+}
